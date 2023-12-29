402b7d026b75f915e8e0f15972d6f0fdd462a3e4
LucsT <lucas@cozycloud.cc>
2023-12-29 17:58:04 +0100

    chore: Remove Sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-process.env.SENTRY_DSN =
-  process.env.SENTRY_DSN ||
-  'https://5a6051872c6e4eefb1f056ac422ad4b2@sentry.cozycloud.cc/134'
-
 const { BaseKonnector } = require('cozy-konnector-libs')
 const { start } = require('./connector')
 
